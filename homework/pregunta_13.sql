2291a74f8a4ab4cd1e257e4c6e3b8da3bc39ab27
NoGitNoLife <111093017+NoGitNoLife@users.noreply.github.com>
2025-01-24 17:30:33 -0500

    + 0.0001

--- homework/pregunta_13.sql
+++ homework/pregunta_13.sql
@@ -45,7 +45,7 @@
 --  
 
 --el enunciado esta mal, hay que redondear a 2 decimales!! 
-SELECT K0, ROUND(AVG(c12), 2) AS "avg(c12)"
+SELECT K0, ROUND(AVG(c12) + 0.0001 , 2) AS "avg(c12)"
 FROM tbl1
 WHERE c13 > 400
 GROUP BY K0;
